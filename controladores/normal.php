2210a2f976c2495d0f4281dc6f3143b257d9c105
Daniel Martin Arnedo <danielmartinarnedoas@gmail.com>
2025-04-16 18:24:41 +0200

    ArregladoLoginRegistro

--- controladores/normal.php
+++ controladores/normal.php
@@ -1,41 +1,46 @@
 <?php
-    //Va hacia Index
-    function irIndex(){
-        header("Location: ../vista/normal/index.php");
-    }
-      //Introduce un usuario y te envia a la lista
-      function insertarUsuario(){
-        $nom = $_POST["nom"];
-        $contra1 = $_POST["contra"];
-        $contra2 = $_POST["contra2"];
-        $mail = $_POST["mail"];
-        require_once("../classes/usuario.php");
-        $usus = new usuario("../../../");
-        $pattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,128}$/";
-        if (preg_match($pattern, $contra1)) {
-            if ($contra1 != $contra2) {
-                $usus->insertUser($nom, $contra, $mail);
-                header("Location: ../vista/normal/index.php");
-            }else {
-                echo "<script>alert('Las contreseñas tienen que ser iguales.');</script>";
-            } 
+//Va hacia Index
+function irLanding()
+{
+    header("Location: ../vista/normal/landing.php");
+}
+//Introduce un usuario y te envia a la lista
+function insertarUsuario()
+{
+    $nom = $_POST["nom"];
+    $contra1 = $_POST["contra"];
+    $contra2 = $_POST["contra2"];
+    $mail = $_POST["mail"];
+    require_once("../classes/usuario.php");
+    $usus = new usuario("../../../");
+    $pattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
+    if (preg_match($pattern, $contra1)) {
+        if ($contra1 === $contra2) {
+            echo "<script>alert('Todo bien');</script>";
+            $usus->insertUser($nom, $contra1, $mail);
+            header("Location: ../index.php");
         } else {
-            echo "<script>alert('La contraseña tiene que tener al menos 1 numero, 1 mayuscula, 1 minuscula y 8 caracteres');</script>";
+            echo "<script>alert('Las contreseñas tienen que ser iguales.');</script>";
+            header("Location: ../vista/normal/introUser.php?action=Uno");
         }
-        header("Location: ../vista/normal/introUser.php");
-        
+    } else {
+        echo "<script>alert('La contraseña tiene que tener al menos 1 numero, 1 mayuscula, 1 minuscula y 8 caracteres');</script>";
+        header("Location: ../vista/normal/introUser.php?action=Dos");
     }
-    //Cerrar Session
-    function cerrarSes(){
-        require_once("../modelo.php");
-        unset_session("user");
-        header("Location: ../vista/normal/index.php");
-    }
-    //Maneja las acciones enviadas por el usuario
-    if (isset($_REQUEST["action"])) {
-        $action = $_REQUEST["action"];
-        $action();
-    }else{
-        header("Location: ./normal.php?action=irIndex");
-    }
+    
+}
+//Cerrar Session
+function cerrarSes()
+{
+    require_once("../modelo.php");
+    unset_session("user");
+    header("Location: ../vista/normal/index.php");
+}
+//Maneja las acciones enviadas por el usuario
+if (isset($_REQUEST["action"])) {
+    $action = $_REQUEST["action"];
+    $action();
+} else {
+    header("Location: ./normal.php?action=irLanding");
+}
 ?>
--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    usuarioDiv=document.getElementById("usuarioHeader");
+    usuarioDiv = document.getElementById("usuarioHeader");
     fetch(`/Frikeria/controladores/src.php?action=conseguirUsuario`, {
         method: "POST",
         headers: {
@@ -10,7 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             console.log(data);
             if (data !== null) {
-                
+                usuarioDiv.innerHTML = `
+                <div class="btn-group">
+                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Right-aligned menu example</button>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><button class="dropdown-item" type="button">Action</button></li>
+                        <li><button class="dropdown-item" type="button">Another action</button></li>
+                        <li><button class="dropdown-item" type="button">Something else here</button></li>
+                    </ul>
+                </div>
+                `;
+
             }
         });
 })   
