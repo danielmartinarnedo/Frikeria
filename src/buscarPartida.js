9d98aa7eef85b93c1f8118601289777708f700a1
Daniel Martin Arnedo <danielmartinarnedoas@gmail.com>
2025-05-03 18:21:04 +0200

    anuncioHecho

--- src/buscarPartida.js
+++ src/buscarPartida.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {    //VARIABLES
-    var permisos = false;
+document.addEventListener('DOMContentLoaded', function () {
+    // VARIALBLES
+    const container = document.getElementById('partidas-container');
     //FUNCIONES
-    //CODIGO GENERAL
-    // Solicitar permiso para obtener la ubicación del usuario
+
+    //CODIGO GENERICO
+
+    //CARGARS LAS PARTIDAS EN EL CONTENEDOR DESDE LA BASE DE DATOS
+    // Se obtiene la ubicación del usuario y se envía una solicitud al servidor para buscar partidas cercanas
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                // Centrar el mapa en la ubicación del usuario
-                let lat= position.coords.latitude
-                let lng= position.coords.longitude
+                let lat = position.coords.latitude;
+                let lng = position.coords.longitude;
+
                 fetch('../controladores/normal.php', {
                     method: 'POST',
                     headers: {
@@ -21,14 +25,37 @@ document.addEventListener('DOMContentLoaded', function () {    //VARIABLES
                     })
                 })
                     .then(response => response.json())
-                    .then(data => {
-                        console.log(data);
+                    .then(datos => {
+
+                        datos.forEach(partida => {
+                            const row = document.createElement('div');
+                            row.className = 'row justify-content-center mb-4';
+                        
+                            row.innerHTML =`
+                            <div class="col-12 mt-3 pb-1 pt-1 border bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="img-fluid h-100" src="${partida.portada}" alt="Portada de ${partida.titulo}" srcset="">
+                    </div>
+                    <div class="col-8">
+                        <h3>${partida.titulo}</h3>
+                        <h4>${partida.juego}</h4>
+                        <p>Número de jugadores: ${partida.numJugadores}</p>
+                        <p>Fecha: ${partida.fecha}</p>
+                        <p>Ciudad: ${partida.ciudad}</p>
+                        <p>${partida.descripcion}</p>
+                        <button class="btn btn-primary col-12" href="./verPartida.php?titulo=${encodeURIComponent(partida.titulo)}&juego=${encodeURIComponent(partida.juego)}&jugadores=${partida.numJugadores}&fecha=${partida.fecha}&ciudad=${encodeURIComponent(partida.ciudad)}&descripcion=${encodeURIComponent(partida.descripcion)}&portada=${encodeURIComponent(partida.portada)}">ENVIAR</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+                            `;
+                        
+                            container.appendChild(row);
+                        });
                     })
                     .catch(error => console.error('Error:', error));
-
-                permisos = true;
-
-                console.log('Ubicación del usuario:', lat, lng);
             },
             function (error) {
                 console.error('Error al obtener la ubicación:', error.message);
@@ -36,6 +63,5 @@ document.addEventListener('DOMContentLoaded', function () {    //VARIABLES
         );
     } else {
         console.error('La geolocalización no es compatible con este navegador.');
-        
     }
 });
--- vista/buscarPartida.php
+++ vista/buscarPartida.php
@@ -1,2 +1,13 @@
+<?php
+require_once("../vista/header.php");
+?>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQRBU4caqOv1t1Fi3NuI9ZlG8Eb9oV9mY&libraries=places"></script>
+<main class="container">
+    <section id="partidas-container" class="row d-flex justify-content-center align-items-center">
+        
+    </section>
+</main>
 <script src="../src/buscarPartida.js"></script>
+<?php
+require_once("../vista/footer.php");
+?>
